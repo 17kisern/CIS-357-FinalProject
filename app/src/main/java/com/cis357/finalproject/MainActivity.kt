0ac42336696e226686f80bb85fffe9a3072ca7c0
Stephen Kiser <17kisern@gmail.com>
2020-12-11 12:59:47 -0500

    Integrated Amplify and have a working app that allows for login and logout, password change, and account creation

--- app/src/main/java/com/cis357/finalproject/MainActivity.kt
+++ app/src/main/java/com/cis357/finalproject/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.cis357.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.amazonaws.mobile.client.AWSMobileClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // UI
+        val greeting = findViewById<TextView>(R.id.usernameText)
+        val signOutButton = findViewById<Button>(R.id.signOutButton)
+
+
+        greeting.text = AWSMobileClient.getInstance().username
+
+        signOutButton.setOnClickListener {
+            AWSMobileClient.getInstance().signOut()
+            greeting.text = "Guest"
+
+            val i =
+                Intent(this@MainActivity, AuthenticationActivity::class.java)
+            startActivity(i)
+        }
     }
 }
